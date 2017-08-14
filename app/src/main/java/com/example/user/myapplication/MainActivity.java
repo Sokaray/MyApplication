78fe4a7663db89ff9fff597aec73ee270cd0a2cc
Sokaray <omegaprime@live.ru>
2017-08-14 22:39:16 +0300

    test5

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -11,7 +11,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-    private boolean test45 = true;
+    private boolean test45 = true;bb
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
