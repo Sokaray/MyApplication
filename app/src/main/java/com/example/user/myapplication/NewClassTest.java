61acbfbfeb57b9fb5fea0345902e569b9279eaad
Sokaray <omegaprime@live.ru>
2017-08-19 10:27:14 +0300

    test8

---
+++ app/src/main/java/com/example/user/myapplication/NewClassTest.java
@@ -0,0 +1,14 @@
+package com.example.user.myapplication;
+
+import android.content.DialogInterface;
+
+/**
+ * Created by User on 19.08.2017.
+ */
+
+public class NewClassTest implements DialogInterface.OnClickListener {
+    @Override
+    public void onClick(DialogInterface dialogInterface, int i) {
+
+    }
+}
